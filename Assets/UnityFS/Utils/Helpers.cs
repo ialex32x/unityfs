308c8cec6beaf6470f28027b87090b00c96d08e3
huliangjie <lin_032@hotmail.com>
2019-07-03 12:31:41 +0800

    helper. OpenManifest (stub)

--- Assets/UnityFS/Utils/Helpers.cs
+++ Assets/UnityFS/Utils/Helpers.cs
@@ -9,6 +9,31 @@ namespace UnityFS.Utils
 
     public static class Helpers
     {
+        public static bool OpenManifest(string filePath, string checksum, out Manifest manifest)
+        {
+            manifest = null;
+            return false;
+            // var fullPath = Path.Combine(_localPathRoot, bundle.name);
+            // var metaPath = fullPath + Metadata.Ext;
+            // if (File.Exists(fullPath))
+            // {
+            //     if (File.Exists(metaPath))
+            //     {
+            //         var json = File.ReadAllText(metaPath);
+            //         var metadata = JsonUtility.FromJson<Metadata>(json);
+            //         // quick but unsafe
+            //         if (metadata.checksum == bundle.checksum && metadata.size == bundle.size)
+            //         {
+            //             var fileStream = System.IO.File.OpenRead(fullPath);
+            //             bundle.Load(fileStream); // 生命周期转由 UAssetBundleBundle 管理
+            //             return true;
+            //         }
+            //         File.Delete(metaPath);
+            //     }
+            // }
+            // return false;
+        }
+
         public static IList<string> URLs(params string[] urls)
         {
             return new List<string>(urls);
