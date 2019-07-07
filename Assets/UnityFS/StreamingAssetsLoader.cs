5146d1e6a23f09020ca46cf7b83796a3757d2f27
huliangjie <lin_032@hotmail.com>
2019-07-07 09:30:48 +0800

    StreamingAssetsLoader (stub)

---
+++ Assets/UnityFS/StreamingAssetsLoader.cs
@@ -0,0 +1,14 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace UnityFS
+{
+    using UnityEngine;
+
+    //TODO: 从streamingassets目录获取bundle
+    public class StreamingAssetsLoader : MonoBehaviour
+    {
+
+    }
+}
